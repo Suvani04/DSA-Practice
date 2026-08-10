1ae615a53b64673195dd74b6e33637bc020edfd6
suvani <suvaniaher04@gmail.com>
2026-08-10 21:38:29 +0530

    Time: 20 ms (38.23%), Space: 108.3 MB (28.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // for(int i=0; i<nums.length;i++){
+        //     for(int j=i+1; j<nums.length;j++){
+        //         if(nums[i]== nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;/
+        HashSet<Integer> set= new HashSet<>();
+        for(int x : nums){
+            if(set.contains(x)){
+                return true;
+            }
+            set.add(x);
+        }
+        return false;
+    }
+}
